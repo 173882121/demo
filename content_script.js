4e753225f297976b236edf1a2d005c1a60be576a
173882121 <173882121@qq.com>
2018-09-19 09:36:03 +0800

    Add files via upload

---
+++ content_script.js
@@ -0,0 +1,46 @@
+﻿/*window.onload = function(){  
+    var username=document.getElementById("input1");  
+    var pwd=document.getElementById("input2");  
+
+    username.value="1502303020";
+    pwd.value="195211";
+    document.getElementById("signin").click();  
+
+} */
+/*window.onload = function(){  
+    var list=document.getElementsByTagName("input");  
+    alert(list.length);
+    for(var i=0;i<list.length;i++)  
+    {  
+        alert(list[i].id);
+    }  
+}  */
+
+window.onload = function () {
+
+    var username = document.getElementsByName("DDDDD");
+    
+        for (let i = 0; i < username.length; i++) {
+            username[i].value = "1502303020";
+             /* username[i].value = "jwcz";*/
+        }
+    
+
+    
+        var pwd = document.getElementsByName("upass");
+        for (let j  = 0;j < pwd.length; j++) {
+            pwd[j].value = "195211";
+             /* pwd[j].value = "ROSE626492";*/
+        }
+    
+   
+
+   
+   
+    var btn = document.getElementsByName("0MKKey");
+    for (let k = 0; k < btn.length; k++) {
+      btn[k].click();
+    }
+    
+
+} 
---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+    document.getElementById("btn").onclick = function () {
+     alert("dddd");
+       // var account = document.getElementById("account").value;
+       // var pwd = document.getElementById("pwd").value;
+       // alert(account);
+    }
